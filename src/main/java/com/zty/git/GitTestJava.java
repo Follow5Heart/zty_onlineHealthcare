d7ee77a9cd2b4e714a1db37f5185180701010c92
ztyCode <1793959949@qq.com>
2022-11-25 00:51:14 +0800

    第三次在idea中提交代码

--- src/main/java/com/zty/git/GitTestJava.java
+++ src/main/java/com/zty/git/GitTestJava.java
@@ -6,5 +6,6 @@ public class GitTestJava {
     public static void main(String[] args) {
         System.out.println("git提交到本地库");
         System.out.println("添加一行代码");
+        System.out.println("1111111");
     }
 }
